069b7ad1726850da6c9ae0830d9ea6b8630b7586
andelamarinovic <andjela.marinovic@kiwi.com>
2023-03-10 13:29:53 +0100

    fixing fixure scopes

--- conftest.py
+++ conftest.py
@@ -1,21 +1,69 @@
 import pytest
-from page.webpage import WebPage, get_page
-from playwright.sync_api import sync_playwright
+from page.webpage import WebPage
+from playwright.sync_api import ViewportSize
+from browser import Browser, Browsers
+from playwright.sync_api import (
+    Browser as PW_Browser,
+    BrowserContext,
+    Playwright,
+    sync_playwright,
+)
+
 # pylint: disable=redefined-outer-name
 
 
+@pytest.fixture(scope="session")
+def playwright():
+    pw = sync_playwright().start()
+    yield pw
+    pw.stop()
+
+
+@pytest.fixture(scope="session")
+def browser(playwright: Playwright):
+    browser_config = Browser("chrome")
+    browser_name = browser_config.capabilities["browserName"]
+
+    if browser_name == Browsers.CHROME:
+        browser = playwright.chromium.launch(headless=False)
+    elif browser_name == Browsers.FIREFOX:
+        browser = playwright.firefox.launch()
+    elif browser_name == Browsers.SAFARI:
+        browser = playwright.webkit.launch()
+    else:
+        browser = None
+
+    yield browser
+
+    browser.close()
+
+
 @pytest.fixture(scope="function")
-def page():
+def browser_context(browser: PW_Browser):
+    browser_config = Browser("chrome")
+    viewport_width = browser_config.screen_width
+    viewport_height = browser_config.screen_height
 
-    with sync_playwright() as pw_context:
+    # Get browser context
+    # Should add user agent explicitly cause without defining page cannot be fully loaded in Chrome headless
+    browser_context = browser.new_context(
+        viewport=ViewportSize(width=viewport_width, height=viewport_height),
+        user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+        )
 
-        browser_context, browser, playwright_page = get_page(pw_context)
-        playwright_page.navigate("https://www.kiwi.com/en/")
+    yield browser_context
 
-        yield playwright_page
+    browser_context.close()
 
-        browser_context.close()
-        browser.close()
+
+@pytest.fixture(scope="function")
+def page(browser_context: BrowserContext):
+    page = browser_context.new_page()
+
+    playwright_page = WebPage(page._impl_obj)
+    playwright_page.navigate("https://www.kiwi.com/en/")
+
+    yield playwright_page
 
 
 @pytest.fixture(autouse=True)
